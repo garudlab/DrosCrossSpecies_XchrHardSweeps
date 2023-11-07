9c0ccff175e8942073c59b4a4c7014ec9b49c354
Mariana Harris <marianaharris94@gmail.com>
2023-11-07 13:22:19 -0800

    updaded autosome files

---
+++ Simulations/SevereShortBottleneck_BGS_MU/run_Bottleneck_A.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script takes in an MS file  with 1000 simulations, and outputs a concatenated processed file. 
+file=$1
+rho_in=$2
+Ne=$3
+Q=$4
+chr=$5
+burn_in=$6
+id=$7
+Mu=$8
+qb=$9
+qd=${10} 
+Tb=${11} 
+Td=${12}
+sexRatio=0.5
+
+outFile=outFiles/Stats_Bottleneck_BGS80_${Ne}_Q${Q}_qb${qb}_Tb${Tb}_$chr_${id}.txt
+
+for i in `seq 1 50`; do #1100
+
+	#runSlim
+	slim -d "file_path='${file}'" -d R=${rho_in}  -d N=${Ne} -d id=${id} -d Mu=${Mu}\
+    	-d Q=${Q}  -d "ChrType='${chr}'" -d burn_in=${burn_in} -d sexRatio=${sexRatio} \
+    	-d qd=${qd} -d qb=${qb} -d tb=${Tb} -d td=${Td} Bottleneck_BGS_A.slim
+
+     python popgenStats_sims.py -i ${file}  -o ${outFile}
+
+done
+rm tmpFiles/*_${id}.txt
